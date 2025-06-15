64c44e1e9c2afa61f7df65328a4515da6928a5a3
jishnujanan <jishnumadappalli@gmail.com>
2025-06-15 20:36:25 +0530

    login created succesfully

--- Fido/src/com/javacard/fido/FidoApplet.java
+++ Fido/src/com/javacard/fido/FidoApplet.java
@@ -11,6 +11,7 @@ import javacard.security.KeyBuilder;
 import javacard.security.KeyPair;
 import javacard.security.PrivateKey;
 import javacard.security.RandomData;
+import javacard.security.Signature;
 
 /**
  * Applet class
@@ -58,6 +59,40 @@ class FidoApplet extends Applet {
 	// Current card state variable (initialized to STATE_INACTIVE by default)
 	private byte CARD_STATE = STATE_INACTIVE;
 
+
+	// Tag for Credential ID
+	private static final short TAG_CREDENTIAL_ID 		= (short)0x4349;
+	
+	// Size of Credential ID
+	private static final byte SIZEOF_CREDENTIAL_ID 		= (byte)0x04;
+	
+	// Tag for Public Key
+	private static final short TAG_PUBLIC_KEY			= (short)0x4B59;
+	
+	// Size of Public Key
+	private static final byte SIZEOF_PUBLIC_KEY			= (byte)0x41;
+	
+	// Tag for Key Details
+	private static final short TAG_KEY_DETAILS	 		= (short)0xEC10;
+	
+	// Size of Key Details
+	private static final byte SIZEOF_KEY_DETAILS	 	= (byte)0x01;
+	
+	// Tag for Signature (the actual digital signature bytes) 'SG"
+	private static final short TAG_SIGNATURE 			= (short)0x5347;
+
+	// Tag for Signature Algorithm (e.g., RSA, ECDSA) 'SA'
+	private static final short TAG_SIGNATURE_ALGORITHM 	= (short)0x5341; 
+	
+	// Size of Signature Algorithm (e.g., RSA, ECDSA) 'SA'
+	private static final byte	 SIZEOF_SIGNATURE_ALGORITHM 	= (byte)0x01; 
+
+	// Tag for Challenge (random nonce used for anti-replay) 'CH'
+	private static final short TAG_CHALLENGE 			= (short)0x4348; 
+
+	// Size of Challenge (random nonce used for anti-replay) 'CH'
+	private static final byte SIZEOF_CHALLENGE 			= (byte)0x08; 
+	
 	// PIN and Credential Handling
 
 	// OwnerPIN object for managing user PIN
@@ -73,7 +108,7 @@ class FidoApplet extends Applet {
 	private byte[] credentialId = null;
 
 	// Total allocated space for credential IDs (supports 100 credentials)
-	private static short sizeOfcredentialId = (short) (4 * 100);
+	private static short sizeOfcredentialIdArray = (short) (4 * 100);
 
 	// Points to the top of the credential ID stack (next free index)
 	private byte credentialIdReferenceTop = (byte) 0;
@@ -81,14 +116,11 @@ class FidoApplet extends Applet {
 	// Transient array for computation (scratch space)
 	private byte[] transientArrayForComputation = null;
 
-	// Size of each credential (in bytes)
-	private static byte SIZEOF_CRED = (byte) 4;
-
-	// Index to the top of credential reference stack
-	private static byte INDEX_CRED_REF_TOP = (byte) 0;
-
 	// Index to generate new credential
-	private static byte INDEX_GEN_CRED = (byte) 1;
+	private static final byte INDEX_GEN_CRED = (byte) 0;
+	
+	// Index to generate new credential
+	private static final byte INDEX_SIGNATURE = (byte)(INDEX_GEN_CRED + SIZEOF_CREDENTIAL_ID);
 
 	// Key Management
 
@@ -101,6 +133,7 @@ class FidoApplet extends Applet {
 	// Array of private keys for storing multiple credentials
 	private PrivateKey[] privateKeys = null;
 
+	private static Signature signature = null;
 	// APDU Case Definitions
 
 	// APDU Case 1: No data in, no data out
@@ -115,6 +148,9 @@ class FidoApplet extends Applet {
 	// APDU Case 4: Data in, data out
 	private static final byte CASE4 = (byte) 0x04;
 
+	//Tags
+	
+	
 	/**
 	 * Installs this applet.
 	 * 
@@ -135,19 +171,21 @@ class FidoApplet extends Applet {
 		ownerPIN = new OwnerPIN(PIN_TRY_LIMIT, MAX_PIN_SIZE);
 
 		// Allocate memory for storing credential IDs
-		credentialId = new byte[sizeOfcredentialId];
+		credentialId = new byte[sizeOfcredentialIdArray];
 
 		// Initialize the random number generator using true random number generator (TRNG)
 		randomData = RandomData.getInstance(RandomData.ALG_TRNG);
 
 		// Create a transient byte array for computation, cleared on card reset
-		transientArrayForComputation = JCSystem.makeTransientByteArray((short)(SIZEOF_CRED + 1), JCSystem.CLEAR_ON_RESET);
+		transientArrayForComputation = JCSystem.makeTransientByteArray((short)(100), JCSystem.CLEAR_ON_RESET);
 
 		// Initialize an EC key pair with a 256-bit key size over a prime field
 		keyPair = new KeyPair(KeyPair.ALG_EC_FP, KeyBuilder.LENGTH_EC_FP_256);
 
 		// Create an array to hold private keys, one per credential (4 bytes per credential)
-		privateKeys = new PrivateKey[(short)(sizeOfcredentialId >> 2)];
+		privateKeys = new PrivateKey[(short)(sizeOfcredentialIdArray >> 2)];
+		
+		signature = Signature.getInstance(Signature.ALG_ECDSA_SHA_256, false);
 		
 		register(bArray, ((short) (bOffset + 1)), bArray[bOffset]);
 	}
@@ -216,8 +254,101 @@ class FidoApplet extends Applet {
 	}
 
 	private void loginProcessing(APDU apdu) {
-		// TODO Auto-generated method stub
 		
+		//Login is a CASE4 command.
+		//The data should be in following format
+		//TAG_CREDENTIAL_ID(2byte) : LENGTH(1 byte) : VALUE(4bytes)
+		//TAG_CHALLENGE(2byte) : LENGTH(1 byte) : VALUE(8bytes)
+		
+		receiveAndSend(apdu, CASE4);
+		
+		if(!ownerPIN.isValidated())
+		{
+			ISOException.throwIt(ISO7816.SW_FUNC_NOT_SUPPORTED);
+		}
+		byte[] apduBuffer = apdu.getBuffer();
+		short dataStartOffset = (short)ISO7816.OFFSET_CDATA;
+		short credentialIdOffset = (short)0;
+		short challengeOffset = (short)0;
+		
+		if(Util.getShort(apduBuffer, dataStartOffset) == TAG_CREDENTIAL_ID)
+		{
+			dataStartOffset += (short)2;
+			if(apduBuffer[dataStartOffset] == SIZEOF_CREDENTIAL_ID)
+			{
+				dataStartOffset++;
+				credentialIdOffset = dataStartOffset;
+				dataStartOffset += SIZEOF_CREDENTIAL_ID;
+				if(Util.getShort(apduBuffer, dataStartOffset) == TAG_CHALLENGE)
+				{
+					dataStartOffset += (short)2;
+					if(apduBuffer[dataStartOffset] == SIZEOF_CHALLENGE)
+					{
+						dataStartOffset++;
+						challengeOffset = dataStartOffset;
+					}
+					else
+					{
+						ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+					}
+				}
+				else
+				{
+					ISOException.throwIt(ISO7816.SW_WRONG_DATA);
+				}
+			}
+			else
+			{
+				ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+			}
+		}
+		else
+		{
+			ISOException.throwIt(ISO7816.SW_WRONG_DATA);
+		}
+		
+		//Data is correct
+		//Find the corresponding key index.
+		short privateKeyOffset = (short)0;
+		short totalCredentialIdSize = (short) (credentialIdReferenceTop*4);
+		while(privateKeyOffset<totalCredentialIdSize)
+		{
+			if(Util.arrayCompare(apduBuffer, credentialIdOffset, credentialId, privateKeyOffset, SIZEOF_CREDENTIAL_ID) == (byte)0)
+			{
+				break;
+			}
+			privateKeyOffset+=SIZEOF_CREDENTIAL_ID;
+		}
+		privateKeyOffset = (short)(privateKeyOffset/4);
+		
+		PrivateKey privateKey = privateKeys[privateKeyOffset];
+		
+		signature.init(privateKey, Signature.MODE_SIGN);
+		
+		short signatureLength = signature.sign(apduBuffer, challengeOffset, SIZEOF_CHALLENGE, transientArrayForComputation, INDEX_SIGNATURE);
+		short outdataOffset = (short)0;
+		
+		Util.setShort(apduBuffer, outdataOffset, TAG_CREDENTIAL_ID);
+		outdataOffset+=(short)2;
+		apduBuffer[outdataOffset] = SIZEOF_CREDENTIAL_ID;
+		outdataOffset++;
+		outdataOffset = Util.arrayCopyNonAtomic(apduBuffer, credentialIdOffset, apduBuffer, outdataOffset, SIZEOF_CREDENTIAL_ID);
+		
+		Util.setShort(apduBuffer, outdataOffset, TAG_SIGNATURE);
+		outdataOffset+=(short)2;
+		apduBuffer[outdataOffset] = (byte) signatureLength;
+		outdataOffset++;
+		outdataOffset = Util.arrayCopyNonAtomic(transientArrayForComputation, INDEX_SIGNATURE, apduBuffer, outdataOffset, signatureLength);
+		
+		Util.setShort(apduBuffer, outdataOffset, TAG_SIGNATURE_ALGORITHM);
+		outdataOffset+=(short)2;
+		apduBuffer[outdataOffset] = SIZEOF_SIGNATURE_ALGORITHM;
+		outdataOffset++;
+		apduBuffer[outdataOffset] = Signature.ALG_ECDSA_SHA_256;
+		outdataOffset++;
+		
+		apdu.setOutgoingLength(outdataOffset);
+		apdu.sendBytes((short)0, outdataOffset);
 	}
 
 	private void registerProcessing(APDU apdu) {
@@ -232,7 +363,7 @@ class FidoApplet extends Applet {
 		}
 		
 		byte[] apduBuffer = apdu.getBuffer();
-		transientArrayForComputation[INDEX_CRED_REF_TOP] = credentialIdReferenceTop;
+		short credentialRefTop = credentialIdReferenceTop;
 		byte cla = apduBuffer[ISO7816.OFFSET_CLA];
 		
 		if(cla!=CLA_PROPRIETARY)
@@ -240,20 +371,20 @@ class FidoApplet extends Applet {
 			ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
 		}
 		
-		if(transientArrayForComputation[INDEX_CRED_REF_TOP] == (byte)(sizeOfcredentialId/4))
+		if(credentialRefTop == (byte)(sizeOfcredentialIdArray/4))
 		{
 			ISOException.throwIt(ISO7816.SW_FILE_FULL);
 		}
 		
 		//Generate a credential ID [4 bytes]
-		randomData.generateData(transientArrayForComputation, INDEX_GEN_CRED, SIZEOF_CRED);
+		randomData.nextBytes(transientArrayForComputation, INDEX_GEN_CRED, SIZEOF_CREDENTIAL_ID);
 		//Check the credential ID generated is duplicate or not.
 		short iter = (short)0;
 		while(iter<(short)(credentialIdReferenceTop*4))
 		{
-			if(Util.arrayCompare(transientArrayForComputation, INDEX_GEN_CRED, credentialId, iter, SIZEOF_CRED) == 0)
+			if(Util.arrayCompare(transientArrayForComputation, INDEX_GEN_CRED, credentialId, iter, SIZEOF_CREDENTIAL_ID) == 0)
 			{
-				randomData.generateData(transientArrayForComputation, INDEX_GEN_CRED, SIZEOF_CRED);
+				randomData.nextBytes(transientArrayForComputation, INDEX_GEN_CRED, SIZEOF_CREDENTIAL_ID);
 				iter=0;
 			}
 			else
@@ -265,7 +396,7 @@ class FidoApplet extends Applet {
 		JCSystem.beginTransaction();
 		
 		//Credential is copied to persistent array
-		Util.arrayCopy(transientArrayForComputation, INDEX_GEN_CRED, credentialId, credentialIdReferenceTop, SIZEOF_CRED);
+		Util.arrayCopy(transientArrayForComputation, INDEX_GEN_CRED, credentialId, credentialIdReferenceTop, SIZEOF_CREDENTIAL_ID);
 
 		//Generation of Key Pair
 		keyPair.genKeyPair();
@@ -281,28 +412,28 @@ class FidoApplet extends Applet {
 		short outData = (short)0;
 		
 		//TLV Coding for Credential ID
-		Util.setShort(apduBuffer, outData, (short)0x4349);
+		Util.setShort(apduBuffer, outData, TAG_CREDENTIAL_ID);
 		outData+=(short)2;
-		apduBuffer[outData] = SIZEOF_CRED;
+		apduBuffer[outData] = SIZEOF_CREDENTIAL_ID;
 		outData++;
-		Util.arrayCopyNonAtomic(transientArrayForComputation, INDEX_GEN_CRED, apduBuffer, outData, SIZEOF_CRED);
-		outData+=(short)SIZEOF_CRED;
+		Util.arrayCopyNonAtomic(transientArrayForComputation, INDEX_GEN_CRED, apduBuffer, outData, SIZEOF_CREDENTIAL_ID);
+		outData+=(short)SIZEOF_CREDENTIAL_ID;
 		
 		//TLV Coding for Public Key Value
-		Util.setShort(apduBuffer, outData, (short)0x4B59);
+		Util.setShort(apduBuffer, outData, TAG_PUBLIC_KEY);
 		outData+=(short)2;
-		apduBuffer[outData] = (byte)65;
+		apduBuffer[outData] = SIZEOF_PUBLIC_KEY;
 		outData++;
 		ECPublicKey publicKey = (ECPublicKey) keyPair.getPublic();
 		publicKey.getW(apduBuffer, outData);
-		outData+=(short)65;
+		outData+=SIZEOF_PUBLIC_KEY;
 		
-		//TLV Coding for Key Type
+		//TLV Coding for Key Details
 		// EC over P-256
-		Util.setShort(apduBuffer, outData, (short)0xEC10);
+		Util.setShort(apduBuffer, outData, TAG_KEY_DETAILS);
 		outData+=(short)2;
-		apduBuffer[outData] = (byte)0x01;
-		outData++;
+		apduBuffer[outData] = SIZEOF_KEY_DETAILS;
+		outData+=SIZEOF_KEY_DETAILS;
 		apduBuffer[outData] = KeyPair.ALG_EC_FP;
 		outData++;
 		apdu.setOutgoingLength(outData);
@@ -444,6 +575,14 @@ class FidoApplet extends Applet {
 			{
 				ne = apdu.setOutgoing();
 				if(ne == (short)0)
+				{
+					ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+				}
+			}
+			else
+			{
+				ne = apdu.setOutgoing();
+				if(ne > (short)0)
 				{
 					ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
 				}
